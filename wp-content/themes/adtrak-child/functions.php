0e830daafe55f066171adff2bdc8bdadd86b5ede
Dan Farrow <dan.farrow@adtrak.co.uk>
2017-03-17 12:37:20 +0000

    + Shortcode added for address_inline and address_stacked

--- wp-content/themes/adtrak-child/functions.php
+++ wp-content/themes/adtrak-child/functions.php
@@ -47,7 +47,6 @@ if (function_exists('acf_add_local_field_group')) {
     include_once __DIR__ . '/includes/acf/buckets.php';
 }
 
-
 /* ========================================================================================================================
 	
 Address - Stacked
@@ -55,20 +54,17 @@ Address - Stacked
 ======================================================================================================================== */
 
 function address_stacked() {
-
 	// loop through the rows of data
 	while ( have_rows('site_address', 'options') ) : the_row();
-
-	// display a sub field value
-	the_sub_field('address_line', 'options');
-
-	echo "<br/>";
+		// display a sub field value
+		the_sub_field('address_line', 'options');
+		echo "<br/>";
 
 	endwhile;
-
 	the_field('site_postcode', 'option');
-
 }
+
+add_shortcode( 'address_stacked', 'address_stacked' );
 
 /* ========================================================================================================================
 	
@@ -77,17 +73,13 @@ Address - Inline
 ======================================================================================================================== */
 
 function address_inline() {
-
 	// loop through the rows of data
 	while ( have_rows('site_address', 'options') ) : the_row();
-
-	// display a sub field value
-	the_sub_field('address_line', 'options');
-
-	echo ",&nbsp;";
-
+		// display a sub field value
+		the_sub_field('address_line', 'options');
+		echo ",&nbsp;";
 	endwhile;
-
 	the_field('site_postcode', 'option');
-
 }
+
+add_shortcode( 'address_inline', 'address_inline' );
