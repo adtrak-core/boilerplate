93f40057ddbce247db06574074200a965b444d2b
Dan Farrow <dan.farrow@adtrak.co.uk>
2018-08-07 13:42:26 +0100

    Plugin & translation updates

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit9922ad94b67f7fe50c9ff4b2c1a53952::getLoader();
+return ComposerAutoloaderInit56290c8d7c9e66f191342ce4e3fd0017::getLoader();
