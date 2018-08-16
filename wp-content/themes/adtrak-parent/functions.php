526b154af83edfba1062896692cd6afa88d5604d
Dan Farrow <dan.farrow@adtrak.co.uk>
2018-08-16 09:54:24 +0100

    Plugin updates & Staging Server noindex fix

--- wp-content/themes/adtrak-parent/functions.php
+++ wp-content/themes/adtrak-parent/functions.php
@@ -160,3 +160,14 @@ function get_adtrak_logo($option = null, $icon = false) {
             break;
     }
 }
+
+/**
+ * Add no index to staging sites
+ */
+add_action('wp_head', function() {
+
+    if (strpos($_SERVER['SERVER_NAME'],'adtrak.agency') !== false) {
+        echo '<meta name="robots" content="noindex">';
+    }
+
+});
