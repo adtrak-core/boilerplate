3c4d40ac57944f294381d65b6932e92d76f8cade
Stephen Greig <steve.greig@adtrak.co.uk>
2019-02-04 11:18:38 +0000

    Site speed updates - optimised loading of scripts and stylesheets (made non-essential files opt-in rather than opt-out and deferred all scripts by default)

--- wp-content/themes/adtrak-child/functions.php
+++ wp-content/themes/adtrak-child/functions.php
@@ -1,37 +1,88 @@
 <?php
 
-/*
- * Enqueue and register scripts the right way.
- */
+/* ========================================================================================================================
+
+Enqueue and register scripts the right way.
+
+======================================================================================================================== */
+
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_script('production', get_theme_file_uri() . '/js/production-dist.js', ['jquery'], '', true);
     wp_enqueue_script('fontawesome', 'https://use.fontawesome.com/731f5cd381.js', [], '', true );
 });
 
-// Deactivate some plugins from some pages
 
-/* 
+
+/* ========================================================================================================================
+	
+Add defer attribute to scripts - ADD TO THIS AS REQUIRED
+	
+======================================================================================================================== */
+
+function add_defer_attribute($tag, $handle) {
+	// add script handles to the array below
+	$scripts_to_defer = array('jquery-core','production','forms-front-js','parsleyjs','adtrak-cookie');
+	 
+	foreach($scripts_to_defer as $defer_script) {
+	   if ($defer_script === $handle) {
+		  return str_replace(' src', ' defer src', $tag);
+	   }
+	}
+	return $tag;
+ }
+ add_filter('script_loader_tag', 'add_defer_attribute', 10, 2);
+
+
+
+/* ========================================================================================================================
+
+Deactivate some plugins from some pages
+
+======================================================================================================================== */
+
 function remove_unwanted_plugins() {
-    // Turn off map other than coverage area
+	/*
+	// Turn off map other than coverage area
     if (!is_page('coverage-area')) {
         remove_action('wp_head', 'bf_head');
         remove_action('wp_footer', 'bf_footer', 30);
-    }
-    // Turn forms other than contact page
+	}
+	*/
+
+    // Only include form scripts on pages that have forms - REMOVE if your forms are on all pages!
     if (!is_page('contact-us')) {
         wp_dequeue_script('parsleyjs');
         wp_dequeue_script('forms-front-js');
-    }
+	}
+
+	// Remove datepicker script - ENABLE if your form has a datepicker field!
+	wp_dequeue_script('jquery-ui-datepicker');
 }
 
 add_action('wp_head', 'remove_unwanted_plugins', 1);
 
-*/
+
+
 
 /* ========================================================================================================================
 	
-Disable Gutenburg
+Deregister Certain Stylesheets
 	
+======================================================================================================================== */
+
+function my_deregister_styles() {
+	wp_deregister_style('forms-front'); // Disable forms stylesheet included by plugin - just use your forms.scss
+	wp_deregister_style('adtrak-cookie'); // Disable separate stylesheet for cookie notice (styles can be found in footer.scss)
+	wp_deregister_style('wp-block-library'); // Gutenberg related stylesheet
+}
+add_action('wp_print_styles', 'my_deregister_styles', 100);
+
+
+
+/* ========================================================================================================================
+
+Disable Gutenberg
+
 ======================================================================================================================== */
 
 add_filter('use_block_editor_for_post', '__return_false');
@@ -39,9 +90,9 @@ add_filter('use_block_editor_for_post', '__return_false');
 
 
 /* ========================================================================================================================
-	
+
 Custom
-	
+
 ======================================================================================================================== */
 
 add_action('after_setup_theme', function () {
@@ -64,9 +115,9 @@ add_action('after_setup_theme', function () {
 
 
 /* ========================================================================================================================
-	
+
 Allow excerpts on pages
-	
+
 ======================================================================================================================== */
 
 add_action( 'init', 'my_add_excerpts_to_pages' );
@@ -77,9 +128,9 @@ function my_add_excerpts_to_pages() {
 
 
 /* ========================================================================================================================
-	
+
 Address - Stacked
-	
+
 ======================================================================================================================== */
 
 function address_stacked() {
@@ -98,9 +149,9 @@ add_shortcode( 'address_stacked', 'address_stacked' );
 
 
 /* ========================================================================================================================
-	
+
 Address - Inline
-	
+
 ======================================================================================================================== */
 
 function address_inline() {
