e51ae8c5b34b86ac38fdd18c4d56670cf1c556fe
Tom Nightingale <thomas.nightingale@gmail.com>
2020-05-11 15:58:43 +0100

    adding mobile menu back in

--- wp-content/themes/adtrak-child-tailwind-twig-timber/_functions/enqueue.php
+++ wp-content/themes/adtrak-child-tailwind-twig-timber/_functions/enqueue.php
@@ -2,4 +2,8 @@
     add_action('wp_enqueue_scripts', function () {
         wp_enqueue_script('production', get_theme_file_uri() . '/js/production-dist.js', ['jquery'], '', true);
         wp_enqueue_script('svgxuse', get_theme_file_uri() . '/js/svgxuse.js', [], '', true );
+        wp_localize_script('production', 'themeURL', array(
+			'themeURL' => get_site_url()
+			)
+		);
     });
--- wp-content/themes/adtrak-child-tailwind-twig-timber/_functions/optimisations.php
+++ wp-content/themes/adtrak-child-tailwind-twig-timber/_functions/optimisations.php
@@ -98,6 +98,14 @@ function address_stacked() {
 }
 add_shortcode('address_stacked', 'address_stacked');
 
+/* ========================================================================================================================
+	Icon Function (show icon from your icons SVG sprite)
+======================================================================================================================== */
+
+function icon($iconName, $classes = null) {
+	echo '<svg class="icon icon-'.$iconName.' '.$classes.'"><use href="'.get_stylesheet_directory_uri().'/images/icons-sprite.svg#icon-'.$iconName.'"></use></svg>';
+}
+
 
 
 
