e11b51941acc830a7c942ca55b8d052acb9a690f
Tom Nightingale <tom.nightingale@adtrak.co.uk>
2020-05-13 10:41:58 +0100

    twig 404

--- wp-content/themes/adtrak-child-tailwind-twig-timber/404.php
+++ wp-content/themes/adtrak-child-tailwind-twig-timber/404.php
@@ -1,29 +1,6 @@
 <?php
-/**
- * The template for displaying 404 pages within your theme.
- * @author  Adtrak
- * @package AdtrakChild
- * @version 1.0.0
- */
 
-get_header(); ?>
+$context = Timber::context();
+$context['post'] = new Timber\Post();
 
-	<main class="site-content container">
-
-		<article class="grid grid12_12">
-
-			<div class="pad-10-10 copy">
-
-				<h1>Sorry!</h1>
-
-				<p>This page can't be found.</p>
-
-				<p><a href="<?php echo site_url('/'); ?>">Please go home</a></p>
-
-			</div>
-
-		</article>
-
-	</main>
-	
-<?php get_footer(); ?>
+Timber::render( [ '404.twig' ], $context );
