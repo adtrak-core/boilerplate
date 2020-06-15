125bf9bd3089608b463cefe895023e6fce2d9695
danfarrowadtrak <dan.farrow@adtrak.co.uk>
2020-06-15 13:39:30 +0100

    Add font in for quick replacement, fixes #24

--- wp-content/themes/adtrak-child-tailwind-twig-timber/tailwind.config.js
+++ wp-content/themes/adtrak-child-tailwind-twig-timber/tailwind.config.js
@@ -2,25 +2,8 @@ module.exports = {
   theme: {
     fontFamily: {
       sans: [
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        '"Noto Sans"',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-        '"Noto Color Emoji"',
-      ],
-      serif: [
-        'Georgia',
-        'Cambria',
-        '"Times New Roman"',
-        'Times',
-        'serif',
+        'Source Sans Pro',
+        'sans-serif'
       ],
     },    
     extend: {
