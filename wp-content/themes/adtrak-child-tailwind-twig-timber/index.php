7376bb24528e627514bcd2fa2161f7ccc133583d
Dan Farrow <dan.farrow@adtrak.co.uk>
2020-07-09 11:27:34 +0100

    Update index.php
    Fix pagination error

--- wp-content/themes/adtrak-child-tailwind-twig-timber/index.php
+++ wp-content/themes/adtrak-child-tailwind-twig-timber/index.php
@@ -1,7 +1,7 @@
 <?php
 
 $context = Timber::context();
-$context['posts'] = Timber::get_posts();
+$context['posts'] = new Timber\PostQuery();
 
 $context['archives'] = wp_get_archives( array('type=>monthly', 'echo'=>0 ));
 $context['categories'] = wp_list_categories(array('title_li' => '', 'echo'=>0));
