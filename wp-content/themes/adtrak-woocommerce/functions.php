348da7d4993ebadc0106f5814eb19203422eb577
unknown <tom.nightingale@adtrak.co.uk>
2020-08-14 15:57:31 +0100

    add scripts from main boilerplate

--- wp-content/themes/adtrak-woocommerce/functions.php
+++ wp-content/themes/adtrak-woocommerce/functions.php
@@ -115,3 +115,12 @@ show_admin_bar(false);
 //         echo '</pre>';
 //     }
 // }
+
+
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_script('production', get_theme_file_uri() . '/dist/production-dist.js', ['jquery'], '', true);
+    wp_localize_script('production', 'themeURL', array(
+        'themeURL' => get_stylesheet_directory_uri()
+        )
+    );
+});
