773e267750127a3acf2e03ff88f06386baae21f7
Dan Farrow <dan@zellement.com>
2021-02-09 10:13:26 +0000

    [refactor] remove parent theme, have one standalone main theme for tw,twig,timber, keep woocommerce, remove nontwig theme

--- wp-content/themes/adtrak-child-tailwind-twig-timber/_functions/register-navigations.php
+++
@@ -1,8 +0,0 @@
-<?php 
-add_action( 'init', 'register_navigations' );
-function register_navigations() {
-     register_nav_menus([
-      'secondary' => __('Secondary Menu', 'adtrak')
-    ]);
-}
-?>
