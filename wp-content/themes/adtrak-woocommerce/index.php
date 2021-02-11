70883b67b0d93b01200dd944546ddccf5498f02c
Tom Nightingale <tom.nightingale@adtrak.co.uk>
2021-02-11 15:24:13 +0000

    update index.twig

--- wp-content/themes/adtrak-woocommerce/index.php
+++ wp-content/themes/adtrak-woocommerce/index.php
@@ -4,10 +4,15 @@ $context = Timber::context();
 
 $context['posts'] = new Timber\PostQuery();
 
-$context['categories'] = get_categories( array(
+$context['categories'] = get_categories([
     'orderby' => 'name',
     'order'   => 'ASC'
-) );
+]);
+
+$context['archives'] = wp_get_archives([
+    'type'            => 'monthly',
+    'echo'            => false,
+]);
 
 $templates = ['index.twig'];
 
