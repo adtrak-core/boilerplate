1bea262e32624c102686f548a60dad7810975d6c
Tom Nightingale <tom.nightingale@adtrak.co.uk>
2021-02-11 13:18:59 +0000

    Close 82

---
+++ wp-content/themes/adtrak-woocommerce/_functions/DeindexStaging.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Add no index to staging sites
+ */
+add_action('wp_head', function() {
+    if (strpos($_SERVER['SERVER_NAME'],'adtrak.agency') !== false) {
+        echo '<meta name="robots" content="noindex">';
+        echo '<meta name="googlebot" content="noindex">';
+    }
+});
+
+?>
